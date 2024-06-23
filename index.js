45bee197254ae27a181fe7ad60353f975f4d7300
JayJetta1 <j_jenkins1@u.pacific.edu>
2024-06-22 20:46:45 -0700

    create function to write README file

--- index.js
+++ index.js
@@ -3,64 +3,92 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = inquirer
-  .prompt([
-    {
-      type: "input",
-      name: "Project Title",
-      message: "What is the title of the project?",
-    },
-    {
-      type: "input",
-      name: "Description",
-      message: "Give a brief description of the product",
-    },
-    {
-      type: "list",
-      name: "Table of Contents",
-      choices: "[Installation, Usage, License, Contributing, Tests, Questions]",
-    },
-    {
-      type: "confirm",
-      name: "Installation",
-      message: "Create your own code?",
-    },
-    {
-      type: "input",
-      name: "Usage",
-      message: "What is the main files being used?",
-    },
-    {
-      type: "input",
-      name: "License",
-      message: "Did you use a license, if so, what license?",
-    },
-    {
-      type: "input",
-      name: "Contributors",
-      message: "List out who or what helped with the product:",
-    },
-    {
-      type: "input",
-      name: "Tests",
-      message: "(Optional) Provide any bugs that were encountered:",
-    },
-    {
-      type: "input",
-      name: "Questions",
-      message:
-        "Provide questions YOU want to be answered for the betterment of the READme genreator:",
-    },
-  ])
-  .then((response) => console.log(response));
-
+const questions = () => {
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "projectTitle",
+        message: "What is the title of the project?",
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "Give a brief description of the product",
+      },
+      {
+        type: "list",
+        name: "tableOfContents",
+        choices: [
+          "Installation",
+          "Usage",
+          "License",
+          "Contributing",
+          "Tests",
+          "Questions",
+        ],
+      },
+      {
+        type: "confirm",
+        name: "installation",
+        message: "Create your own code?",
+      },
+      {
+        type: "input",
+        name: "usage",
+        message: "What is the main files being used?",
+      },
+      {
+        type: "input",
+        name: "license",
+        message: "Did you use a license, if so, what license?",
+      },
+      {
+        type: "input",
+        name: "contributors",
+        message: "List out who or what helped with the product:",
+      },
+      {
+        type: "input",
+        name: "tests",
+        message: "(Optional) Provide any bugs that were encountered:",
+      },
+      {
+        type: "input",
+        name: "questions",
+        message: "Questions? Provide an email or contact:",
+      },
+    ])
+    .then((response) => {
+      console.log(response);
+      return response;
+    })
+    .catch((error) => {
+      console.error("Error during prompt:", error);
+    });
+};
 questions();
+
 // TODO: Create a function to write README file
-function writeToFile(data) {
-  fs.writeFile("readme.md", data, (err) => {
-    err ? console.log("Error writing READme file!") : console.log("Success!");
+function writeToFile() {
+  const content = `
+  # Project Title
+  ## Description
+  ### Table of Contents
+  ### Installation
+  ### Usage
+  ### License
+  ### Contributors
+  ### Tests
+  ### Questions`;
+
+  fs.writeFile("README.md", content, (err) => {
+    err
+      ? console.log("Error writing READme file!")
+      : console.log("READme file successfully created!");
   });
 }
+writeToFile();
 
 // TODO: Create a function to initialize app
 function init() {}
