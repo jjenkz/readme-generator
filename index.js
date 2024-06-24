09852ea48fb0c64717b39c7c57d07d9f1504d4cb
JayJetta1 <j_jenkins1@u.pacific.edu>
2024-06-23 17:17:14 -0700

    cleaning code

--- index.js
+++ index.js
@@ -1,64 +1,78 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
+const markdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = () => {
-  return inquirer
-    .prompt([
-      {
-        type: "input",
-        name: "projectTitle",
-        message: "What is the title of the project?",
-      },
-      {
-        type: "input",
-        name: "description",
-        message: "Give a brief description of the product",
-      },
-      {
-        type: "checkbox",
-        name: "tableOfContents",
-        choices: [
-          "Installation",
-          "Usage",
-          "License",
-          "Contributing",
-          "Tests",
-          "Questions",
-        ],
-      },
-      {
-        type: "confirm",
-        name: "installation",
-        message: "Create your own code?",
-      },
-      {
-        type: "input",
-        name: "usage",
-        message: "What are the main files being used?",
-      },
-      {
-        type: "input",
-        name: "license",
-        message: "Did you use a license, if so, what license?",
-      },
-      {
-        type: "input",
-        name: "contributors",
-        message: "List out who or what helped with the product:",
-      },
-      {
-        type: "input",
-        name: "tests",
-        message: "(Optional) Provide any bugs that were encountered:",
-      },
-      {
-        type: "input",
-        name: "questions",
-        message: "Questions? Provide an email or contact:",
-      },
-    ])
+const questions = [
+  {
+    type: "input",
+    name: "projectTitle",
+    message: "What is the title of the project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Give a brief description of the product",
+  },
+  {
+    type: "checkbox",
+    name: "tableOfContents",
+    message: "Choose for the README file:",
+    choices: [
+      "Installation",
+      "Usage",
+      "License",
+      "Contributing",
+      "Tests",
+      "Questions",
+    ],
+  },
+  {
+    type: "confirm",
+    name: "installation",
+    message: "Create your own code?",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "What are the main files being used?",
+  },
+  {
+    type: "input",
+    name: "license",
+    message: "Did you use a license, if so, what license?",
+  },
+  {
+    type: "input",
+    name: "contributors",
+    message: "List out who or what helped with the product:",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "(Optional) Provide any bugs that were encountered:",
+  },
+  {
+    type: "input",
+    name: "questions",
+    message: "Questions? Provide an email or contact:",
+  },
+];
+
+// TODO: Create a function to write README file
+function writeToFile(questions) {
+  fs.writeFile("README.md", questions, (err) => {
+    err
+      ? console.error("Error writing READme file!")
+      : console.log("READme file successfully created!");
+  });
+}
+writeToFile(questions);
+
+// TODO: Create a function to initialize app
+function init() {
+  inquirer.prompt
     .then((response) => {
       console.log(response);
       return response;
@@ -66,32 +80,7 @@ const questions = () => {
     .catch((error) => {
       console.error("Error during prompt:", error);
     });
-};
-questions();
-
-// TODO: Create a function to write README file
-function writeToFile() {
-  const content = `
-  # Project Title
-  ## Description
-  ### Table of Contents
-  ### Installation
-  ### Usage
-  ### License
-  ### Contributors
-  ### Tests
-  ### Questions`;
-
-  fs.writeFile("README.md", content, (err) => {
-    err
-      ? console.log("Error writing READme file!")
-      : console.log("READme file successfully created!");
-  });
 }
-writeToFile();
-
-// TODO: Create a function to initialize app
-function init() {}
 
 // Function call to initialize app
 init();
